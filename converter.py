2bee40cfd2a77eeb79c463ce4abd9e103725c7d9
MPorlaris <540492239@qq.com>
2022-04-14 14:46:44 +0800

    update readme and usage

--- converter.py
+++ converter.py
@@ -1,6 +1,6 @@
 import os
 import logging
-import numpy as np
+import argparse
 from utils import load_onnx_modelproto, keras_builder, tflite_builder
 
 LOG = logging.getLogger("converter running:")
@@ -15,17 +15,36 @@ def onnx_converter(onnx_model_path:str, need_simplify:bool=True, tflite_out_path
     tflite_model = tflite_builder(keras_model, weight_quant, int8_model, image_root)
 
     if tflite_out_path is None:
-        tflite_model_name = os.path.split(onnx_model_path)[-1]
-        tflite_out_path = "".join(tflite_model_name.split('.')[:-1]) + ".tflite"
+        tflite_out_path, tflite_model_name = os.path.split(onnx_model_path)
+        tflite_out_path = os.path.join(tflite_out_path, "".join(tflite_model_name.split('.')[:-1]) + ".tflite")
+    if os.path.isdir(tflite_out_path):
+        tflite_out_path = os.path.join(tflite_out_path, os.path.split(onnx_model_path)[-1].split(".")[0] + ".tflite")
     with open(tflite_out_path, "wb") as fp:
         fp.write(tflite_model)
     
     LOG.info(f"转换完成, 模型保存在{tflite_out_path}")
 
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str, default='./models/mobilenetv3.onnx', help='onnx model path')
+    parser.add_argument('--outpath', type=str, default='./utils', help='tflite model save path')
+    parser.add_argument('--simplify', default=True, action='store_true', help='onnx model need simplify model')
+    parser.add_argument('--weigthquant', default=False, action='store_true', help='tflite weigth int8 quant')
+    parser.add_argument('--int8', default=False, action='store_true', help='tflite weigth int8 quant, include input output')
+    parser.add_argument('--imgroot', type=str, default=None, help='when int8=True, imgroot should give for calculating mean and norm')
+    opt = parser.parse_args()
+    return opt
+
+def run():
+    opt = parse_opt()
+    onnx_converter(
+        onnx_model_path = opt.weights,
+        need_simplify = opt.simplify,
+        tflite_out_path = opt.outpath,
+        weight_quant=opt.weigthquant,
+        int8_model=opt.int8,
+        image_root=opt.imgroot
+    )
 
 if __name__ == "__main__":
-    onnx_converter(
-        onnx_model_path = "./models/yolov5n.onnx",
-        need_simplify = True,
-        tflite_out_path = "./models/yolov5n.tflite"
-    )
+    run()
--- readme.md
+++ readme.md
@@ -1,17 +1,38 @@
-# ONNX转TFLite模型代码
-## 注意
+# ONNX->TFLite tools
+
+## How to use
+```python
+# base
+python converter --weights "./your_model.onnx"
+
+# give save path
+python converter --weights "./your_model.onnx" --outpath "./save_path"
+python converter --weights "./your_model.onnx" --outpath "./tflitemodel.tflite"
+
+# quantitative model weight, only weight
+python converter --weights "./your_model.onnx" --weigthquant
+
+# quantitative model weight, include input and output
+python converter --weights "./your_model.onnx" --int8 --imgroot "./dataset_path" # recommend
+python converter --weights "./your_model.onnx" --int8 # generate random data, instead of read from image file
+```
+
+## 注意(Caution)
+- please use [comfirm_acc.py](./test/comfirm_acc.py) comfirm output is correct after convertion, because some of methods rely on practice.
+- tools is going on update.
+- only support 2D CNN, may be support more types of CNN or transformer in the future.
 - 因为大部分是靠实践经验的，所以转换完成最好使用[comfirm_acc.py](./test/comfirm_acc.py)确认转换精度。
 - 目前还没有更新完，我需要一点时间。。。
 - 目前只支持2D卷积网络
 ---
 
-## 添加新算子时，API查询地址
+## 添加新算子时，API查询地址(API reference docs)
 - onnx_api : https://github.com/onnx/onnx/blob/main/docs/Operators.md
 - tensorflow_api : https://tensorflow.google.cn/api_docs/python/tf
 - keras_api : https://keras.io/search.html
 ---
 
-## 已验证的模型列表
+## 已验证的模型列表(support models)
 - Resnet
 - Densenet
 - Mobilenet
@@ -20,3 +41,4 @@
 - UNet\FPN
 - YOLOX
 - YOLOV5
+- normal CNN
