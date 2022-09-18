dee84a0afb8d9399bf830045542a05722bfcf41f
MPolaris <540492239@qq.com>
2022-09-18 10:54:22 +0800

    Erf update

--- layers/calculations_layers.py
+++ layers/calculations_layers.py
@@ -201,34 +201,11 @@ class TFArgMin():
             _inputs = tf.expand_dims(_inputs, axis=self.axis)
         return _inputs
 
+@OPERATOR.register_operator("Erf")
 class TFErf():
-    def __init__(self) -> None:
-        '''
-        被注释的部分为完全体,精度高,但计算速度较慢，
-        可以根据需求进行删减
-        '''
-        # self.coefficient = [-1.3026537197817094, 6.4196979235649026e-1,
-        #             1.9476473204185836e-2,-9.561514786808631e-3,-9.46595344482036e-4,
-        #             3.66839497852761e-4,4.2523324806907e-5,-2.0278578112534e-5,
-        #             -1.624290004647e-6,1.303655835580e-6,1.5626441722e-8,-8.5238095915e-8,
-        #             6.529054439e-9,5.059343495e-9,-9.91364156e-10,-2.27365122e-10,
-        #             9.6467911e-11, 2.394038e-12,-6.886027e-12,8.94487e-13, 3.13092e-13,
-        #             -1.12708e-13,3.81e-16,7.106e-15,-1.523e-15,-9.4e-17,1.21e-16,-2.8e-17]
-        self.coefficient = [-1.3026537197817094, 6.4196979235649026e-1,
-                    1.9476473204185836e-2,-9.561514786808631e-3,-9.46595344482036e-4,
-                    3.66839497852761e-4,4.2523324806907e-5,-2.0278578112534e-5]
+    def __init__(self, *args, **kwargs) -> None:
+        pass
     
     def __call__(self, inputs):
-        inputs = tf.where(inputs>=0, 1-self.erfccheb(inputs), self.erfccheb(-inputs)-1)
+        inputs = tf.math.erf(inputs)
         return inputs
-
-    def erfccheb(self, data):
-        data = tf.maximum(data, 0)
-        var1 = 2.0/(2.0 + data)
-        var2 = 4.0*var1 - 2.0
-        var3, var4, var5 = 0.0, 0.0, 0.0
-        for j in range(len(self.coefficient)-1, 0, -1):
-            var3 = var4
-            var4 = var2*var4 - var5 + self.coefficient[j]
-            var5 = var3
-        return var1*tf.exp(-data*data + 0.5*(self.coefficient[0] + var2*var4) - var5)
