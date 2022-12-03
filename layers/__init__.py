cbcabdbc0528c98228c82582233e2df45409e6a2
MPolaris <540492239@qq.com>
2022-12-03 21:14:26 +0800

    rename update

--- layers/__init__.py
+++ layers/__init__.py
@@ -4,5 +4,5 @@ from .conv_layers import *
 from .dimension_utils import *
 from .common_layers import *
 from .activations_layers import *
-from .calculations_layers import *
+from .mathematics_layers import *
 from .deformation_layers import *
